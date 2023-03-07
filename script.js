628c9cb13bd91ec38292c2bc09e7224d3d50eade
Jordan <jordan.castelloe@gmail.com>
2023-03-07 12:06:21 -0500

    data structure for erd workshop

---
+++ script.js
@@ -0,0 +1,69 @@
+const inventory = [
+    {
+        id: 1,
+        name: "Cookbook",
+        price: 19.99,
+        categoryId: 2
+    },
+    {
+        id: 2,
+        name: "Rocking Horse",
+        price: 12.99,
+        categoryId: 1
+    },
+    {
+        id:4,
+        name: "Pair of shoes",
+        price: 79.99,
+        categoryId: 3
+
+    },
+    {
+        id:5,
+        name: "Pair of boots",
+        price: 79.99,
+        categoryId: 3
+
+    },
+    {
+        id:6,
+        name: "Jack in the box",
+        price: 79.99,
+        categoryId: 1
+
+    },
+    {
+        id:7,
+        name: "Blocks",
+        price: 79.99,
+        categoryId: 4
+
+    },
+    {
+        id: 8,
+        name: "Alphabet blocks",
+        price: 19.99,
+        categoryId: 4
+    },
+    {
+        id: 9,
+        name: "Number blocks",
+        price: 17.99,
+        categoryId: 4
+    }
+]
+
+const categories = [
+    {
+        id:1,
+        name: "Children's Goods",
+    },
+    {
+        id: 2,
+        name: "Books"
+    },
+    {
+        id: 3,
+        name: "Foot wear"
+    }
+]
